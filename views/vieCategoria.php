d2c11a43d9e8ab83743735da914b027a78d400ac
TIO-EUYIN <jeugenio680@gmail.com>
2021-01-27 00:07:09 -0800

    VISTAS Y COTNTROLADOR

---
+++ views/vieCategoria.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body class="bg-danger">
+
+<div class="container">
+  <h2>CATEGORIAS</h2>
+  <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+        <th>ESTADOS</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>Doe@example.com</td>
+        <td><button type="button" class="btn btn-success">EDITAR</button></td>
+        <td><button type="button" class="btn btn-primary">ELIMINAR</button></td>
+        <td><button type="button" class="btn btn-warning">ESTADO</button></td>
+
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td><td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+
+</body>
+</html>
